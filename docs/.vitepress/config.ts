1b86ff3a475bc52ffd067d82c4587045753248d7
283375 <bilibili283375@163.com>
2023-11-18 20:10:31 +0800

    init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitepress'
+
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'Arcaea Offline',
+  description: 'Arcaea Offline concepts',
+  themeConfig: {
+    logo: '/logo-icon.inkscape.svg',
+
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+  },
+})
---
+++ docs/index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Arcaea Offline"
+  tagline: "Arcaea Offline 概念文档"
+  image:
+    src: /logo.publish.svg
+    alt: Arcaea Offline logo
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
