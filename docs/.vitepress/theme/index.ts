598d9e308ca6175fb7928af51db42c0109083416
283375 <bilibili283375@163.com>
2025-06-07 22:20:42 +0800

    feat(components): EntityLink

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,12 @@
+import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './custom.css'
 
-export default DefaultTheme
+import './custom.css'
+import EntityLink from '../../components/EntityLink.vue'
+
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('EntityLink', EntityLink)
+  },
+} satisfies Theme
---
+++ docs/components/EntityLink.vue
@@ -0,0 +1,49 @@
+<template>
+  <a class="VPLink link link-with-icon" :href="linkEntry.href">
+    <i class="link-with-icon-icon">
+      <component :is="linkEntry.icon" />
+    </i>
+    <span class="link-with-icon-text">{{ linkEntry.text }}</span>
+  </a>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import links from './entity-links'
+import type { EntityLinkEntry } from './entity-links'
+
+const props = defineProps<{
+  target: string
+}>()
+
+const linkEntry = computed<EntityLinkEntry>(() => {
+  const link = links[props.target]
+
+  if (link) {
+    return link
+  } else {
+    return {
+      text: `<! ${props.target} !>`,
+    }
+  }
+})
+</script>
+
+<style lang="css" scoped>
+.link-with-icon {
+  display: inline-block;
+  text-wrap: nowrap;
+}
+
+.link-with-icon-text {
+  display: inline-block;
+  vertical-align: baseline;
+}
+
+.link-with-icon-icon {
+  display: inline-block;
+  vertical-align: middle;
+  margin-inline-end: 0.2em;
+}
+</style>
